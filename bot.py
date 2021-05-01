ed5fbe09452a4006787fa85ae8e486879d15d5b4
Ethan Kuai <>
2021-05-01 15:09:01 +0000

    basic functionality implemented, can now connect to discord

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 import discord
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
+
+import os
+TOKEN = os.environ['TOKEN']
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} has connected!')
+    print('Alternatively, {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return # bot is not a psycho
+
+    if message.content.startswith('.'):
+        await message.channel.send('Hello!')
 
 client.run(TOKEN)
