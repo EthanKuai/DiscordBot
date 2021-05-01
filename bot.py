5834770cccc9376b083b5312451ac86513000d43
Ethan Kuai <>
2021-05-01 16:53:08 +0000

    TESTED AND FINALISED NEW JSON COMMAND LOOK-UP

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 import requests
 import json
 
-key = "" # command to be run
+key = 0 # command to be run
 exact_keys = json.load(open('exact_keys.json',))
 #start_keys = json.load(open('start_keys.json',))
 
@@ -12,21 +12,16 @@ exact_keys = json.load(open('exact_keys.json',))
 def valid(message):
     tmpkey = message.split(' ', 1)[0].lower()
     if tmpkey in exact_keys:
-        return tmpkey
+        return exact_keys[tmpkey]
     #future start_keys method
     return False
 
-# shortcut
-def s(txt):
-    return txt==key
-
 # quote feature
-QUOTE_MAX_STR = "Too many requests. Obtain an auth key for unlimited access. -zenquotes.io"
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    if quote == QUOTE_MAX_STR: return "On cooldown."
+    if quote.startswith("Too many requests."): return "On cooldown."
     return quote
 
 @client.event
@@ -40,7 +35,7 @@ async def on_message(message):
         return # bot is not a psycho
     
     # validates its a valid command
-    if message.content.startswith('.'):
+    if message.content[0] == ".":   
         txt = message.content[1:] # message
         key = valid(txt)
         if not key:
@@ -49,11 +44,13 @@ async def on_message(message):
             txt = txt.strip().split(' ')
     else: return
     
-    if s('hello') or s('hi') or s('ohayou') or s('bonjour'):
+    if key == 1:
         await message.channel.send('Hello!')
+
+    elif key == 2:
+        await message.channel.send(get_quote())
     
-    elif s('inspire') or s('quote') or s('inspiration'):
-        quote = get_quote()
-        await message.channel.send(quote)
+    elif key == 3:
+        await message.channel.send("there is supposed to be news feeds")
 
 client.run(os.environ['TOKEN'])
